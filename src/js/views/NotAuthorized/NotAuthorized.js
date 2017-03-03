c80ca671a9cca18585b93fcfec593dda121dcea6
Théo Zimmermann <theo.zimmermann@cpnv.ch>
2017-03-03 11:02:31 +0100

    login working

--- src/js/views/NotAuthorized/NotAuthorized.js
+++ src/js/views/NotAuthorized/NotAuthorized.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Box from 'grommet/components/Box';
 import Header from '../../components/Header/';
-class Profile extends React.Component {
+class NotAuthorized extends React.Component {
 
   render() {
 
     return (
       <Box>
         <Header />
-        <h1>CRUD PROJECT</h1>
+        <h1>YOU ARE NOT ALLOWED TO GO THERE </h1>
       </Box>
     );
   };
 };
-export default Profile;
+export default NotAuthorized;
