4f2f1128f84bd1ff3938581db739c89e7e01e1d3
GIRARD Quentin <Quentin.GIRARD@cpnv.ch>
2017-03-20 10:11:54 +0100

    Details of project

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import App from './App';
 import AppView from './views/App/';
 import Login from './views/Login/';
@@ -10,29 +10,26 @@ import AdminArea from './views/AdminArea';
 import UserManagement from './views/UserManagement';
 import UserArea from './views/UserArea';
 import UnAuthorized from './views/UnAuthorized/';
-// import AdminArea from './components/AdminArea';
-// import UserArea from './components/UserArea';
-// import NotAuthorized from './views/NotAuthorized/';
 
 const rootPath = '/';
 
 const routes = (
-  <Router history={browserHistory} >
-    <Route path={rootPath} component={App}>
-      <IndexRoute component={AppView} />
-      <Route path="/login" component={Login} />
-      <Route path="/details/:id" component={Details} />
-      <Route path="/disconnect" component={Disconnect} />
-      <Route authorize={['user']} component={UserArea}>
-        <Route path='/profile' component={Profile} />
-      </Route>
-      <Route authorize={['admin', 'Project manager', 'Administrator', 'administrator']} component={AdminArea}>
-        <Route path='/userManagement' component={UserManagement} />
-      </Route>
+    <Router history={browserHistory}>
+        <Route path={rootPath} component={App}>
+            <IndexRoute component={AppView}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/details/:id" component={Details}/>
+            <Route path="/disconnect" component={Disconnect}/>
+            <Route authorize={['user']} component={UserArea}>
+                <Route path='/profile' component={Profile}/>
+            </Route>
+            <Route authorize={['admin', 'Project manager', 'Administrator', 'administrator']} component={AdminArea}>
+                <Route path='/userManagement' component={UserManagement}/>
+            </Route>
 
-    </Route>
-    <Route path="/unauthorized" component={UnAuthorized} />
-  </Router>
+        </Route>
+        <Route path="/unauthorized" component={UnAuthorized}/>
+    </Router>
 
 );
 
